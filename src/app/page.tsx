bb1eacc362b11ef8e191df9797aedc890ff9d882
Lilian Cristine Semann <126900345+LilianSemann@users.noreply.github.com>
2023-10-25 15:53:00 -0300

    third step

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,50 @@
+"use client"
+
 export default function Home() {
+
+  let player = "X"
+  let table = ["", "", "", "", "", "", "", "", ""]
+
+  function play(index: number) {
+    if (table[index] !== "") return
+    
+    table[index] = player
+    player = player == "X" ? "O" : "X"
+
+    render()
+  }
+
+  function render() {
+    let tds = document.querySelectorAll("td")
+
+    tds.forEach((td, index) => {
+      td.innerHTML = table[index]
+    });
+
+  }
+
   return (
-    <></>
+    <section className="h-screen w-full flex flex-col justify-center items-center gap-2">
+      <h2>TicTacToe 3</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td onClick={() => play(0)} className="w-[60px] h-[60px] text-center border border-black hover:bg-lime-100 text-3xl cursor-pointer"></td>
+            <td onClick={() => play(1)} className="w-[60px] h-[60px] text-center border border-black hover:bg-lime-100 text-3xl cursor-pointer"></td>
+            <td onClick={() => play(2)} className="w-[60px] h-[60px] text-center border border-black hover:bg-lime-100 text-3xl cursor-pointer"></td>
+          </tr>
+          <tr>
+            <td onClick={() => play(3)} className="w-[60px] h-[60px] text-center border border-black hover:bg-lime-100 text-3xl cursor-pointer"></td>
+            <td onClick={() => play(4)} className="w-[60px] h-[60px] text-center border border-black hover:bg-lime-100 text-3xl cursor-pointer"></td>
+            <td onClick={() => play(5)} className="w-[60px] h-[60px] text-center border border-black hover:bg-lime-100 text-3xl cursor-pointer"></td>
+          </tr>
+          <tr>
+            <td onClick={() => play(6)} className="w-[60px] h-[60px] text-center border border-black hover:bg-lime-100 text-3xl cursor-pointer"></td>
+            <td onClick={() => play(7)} className="w-[60px] h-[60px] text-center border border-black hover:bg-lime-100 text-3xl cursor-pointer"></td>
+            <td onClick={() => play(8)} className="w-[60px] h-[60px] text-center border border-black hover:bg-lime-100 text-3xl cursor-pointer"></td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
   )
 }
