da4f38513826731e556aa57742854d90c214a103
Lilian Cristine Semann <Liliansemann2525@gmail.com>
2023-10-27 21:11:02 -0300

    mapeando matriz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,41 @@ import { useState } from "react"
 export default function Home() {
 
   const [player, setPlayer] = useState("X")
-  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""])
+  const [board, setBoard] = useState([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ])
 
-  function play(i: number) {
-    
+  function play(i: number, j: number) {
+    if (board[i][j] !== "") {return}
+
+    const newBoard = board.map((row, rowI) => 
+      row.map((cell, colI) => {
+        if (rowI === i && colI === j) {
+          return player
+        } else {
+          return cell
+        }
+      }))
+
+    setPlayer(player === "X" ? "O" : "X")
+    setBoard(newBoard)    
   }
 
   return (
-    <section className="h-screen w-full flex flex-col justify-center items-center">
+    <section className="h-screen w-full flex flex-col justify-center items-center bg-black">
       <table>
         <tbody>
-          <tr>
-            <td onClick={() => play(0)} className="w-[60px] h-[60px] text-2xl text-center border border-black">{board[0]}</td>
-            <td className="w-[60px] h-[60px] text-2xl text-center border border-black">{board[1]}</td>
-            <td className="w-[60px] h-[60px] text-2xl text-center border border-black">{board[2]}</td>
-          </tr>
-          <tr>
-            <td className="w-[60px] h-[60px] text-2xl text-center border border-black">{board[3]}</td>
-            <td className="w-[60px] h-[60px] text-2xl text-center border border-black">{board[4]}</td>
-            <td className="w-[60px] h-[60px] text-2xl text-center border border-black">{board[5]}</td>
-          </tr>
-          <tr>
-            <td className="w-[60px] h-[60px] text-2xl text-center border border-black">{board[6]}</td>
-            <td className="w-[60px] h-[60px] text-2xl text-center border border-black">{board[7]}</td>
-            <td className="w-[60px] h-[60px] text-2xl text-center border border-black">{board[8]}</td>
-          </tr>
+          {board.map((row, i) => (
+            <tr key={i}>
+              {row.map((cell, j) => (
+                <td key={j} onClick={() => play(i, j)} className="border border-white text-white h-[60px] w-[60px] text-3xl text-center hover:bg-zinc-900 cursor-pointer">
+                  {cell}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </section>
