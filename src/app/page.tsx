3eae23f4895cf5150f5a87802da6a5e19f484880
Lilian Cristine Semann <Liliansemann2525@gmail.com>
2023-11-25 01:31:34 -0300

    almost done!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,68 @@
 "use client"
 
+import Image from "next/image"
 import { useState } from "react"
 
 export default function Home() {
 
-  const [player, setPlayer] = useState("X")
-  const [board, setboard] = useState(["", "", "", "", "", "", "", "", ""])
+  const [player, setPlayer] = useState("First")
+  const [board, setboard] = useState(["Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null"]) 
 
   function play(index: number) {
-    if (board[index] != "") return
+    if (board[index] != "Null") return
 
     board[index] = player
     setboard([...board])
-    setPlayer(player === "X" ? "O" : "X")
+    setPlayer(player === "First" ? "Second" : "First")
     check()
   }
 
   function check() {
-    if (board[0] == board[1] && board[1] == board[2] && board[2] != "") alert("player " + player + " won!")
-    if (board[3] == board[4] && board[4] == board[5] && board[5] != "") alert("player " + player + " won!")
-    if (board[6] == board[7] && board[7] == board[8] && board[8] != "") alert("player " + player + " won!")
-    if (board[0] == board[4] && board[4] == board[8] && board[8] != "") alert("player " + player + " won!")
-    if (board[2] == board[4] && board[4] == board[6] && board[6] != "") alert("player " + player + " won!")
-    if (board[0] == board[3] && board[3] == board[6] && board[6] != "") alert("player " + player + " won!")
-    if (board[1] == board[4] && board[4] == board[7] && board[7] != "") alert("player " + player + " won!")
-    if (board[2] == board[5] && board[5] == board[8] && board[8] != "") alert("player " + player + " won!")
-  }
+    const audio = document.querySelectorAll('audio')
+    
+    if (board[0] == board[1] && board[0] == board[2] && board[0] != "Null") {audio[1].play(); setboard(["Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null"]); setPlayer("First"); return} 
+    if (board[3] == board[4] && board[3] == board[5] && board[3] != "Null") {audio[1].play(); setboard(["Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null"]); setPlayer("First"); return}
+    if (board[6] == board[7] && board[6] == board[8] && board[6] != "Null") {audio[1].play(); setboard(["Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null"]); setPlayer("First"); return}
+    if (board[0] == board[4] && board[0] == board[8] && board[0] != "Null") {audio[1].play(); setboard(["Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null"]); setPlayer("First"); return}
+    if (board[2] == board[4] && board[2] == board[6] && board[2] != "Null") {audio[1].play(); setboard(["Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null"]); setPlayer("First"); return}
+    if (board[0] == board[3] && board[3] == board[6] && board[6] != "Null") {audio[1].play(); setboard(["Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null"]); setPlayer("First"); return}
+    if (board[1] == board[4] && board[4] == board[7] && board[7] != "Null") {audio[1].play(); setboard(["Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null"]); setPlayer("First"); return}
+    if (board[2] == board[5] && board[5] == board[8] && board[8] != "Null") {audio[1].play(); setboard(["Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null"]); setPlayer("First"); return}
 
+    if (board[0] != "Null" && board[1] != "Null" && board[2] != "Null" && board[3] != "Null" && board[4] != "Null" && board[5] != "Null" && board[6] != "Null" && board[7] != "Null" && board[8] != "Null") {
+       audio[0].play(); setboard(["Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null", "Null"])}
+  }
+    
   return (
-    <section className="h-screen w-full bg-black flex justify-center items-center">
-      <table>
+    <section className="h-screen w-full flex flex-col items-center bg-[url('/background.jpg')] bg-cover">
+      <Image
+        src={'/title.png'}
+        alt="title"
+        width={409}
+        height={149}
+        className="mt-4"
+      />
+      <table className="mt-4">
         <tbody>
           <tr>
-            <td onClick={() => play(0)} className="border border-white h-[70px] w-[70px] text-white text-center text-4xl hover:bg-zinc-700 cursor-pointer">{board[0]}</td>
-            <td onClick={() => play(1)} className="border border-white h-[70px] w-[70px] text-white text-center text-4xl hover:bg-zinc-700 cursor-pointer">{board[1]}</td>
-            <td onClick={() => play(2)} className="border border-white h-[70px] w-[70px] text-white text-center text-4xl hover:bg-zinc-700 cursor-pointer">{board[2]}</td>
+            <td onClick={() => play(0)} className={`${board[0]}`}></td>
+            <td onClick={() => play(1)} className={`${board[1]}`}></td>
+            <td onClick={() => play(2)} className={`${board[2]}`}></td>
           </tr>
           <tr>
-            <td onClick={() => play(3)} className="border border-white h-[70px] w-[70px] text-white text-center text-4xl hover:bg-zinc-700 cursor-pointer">{board[3]}</td>
-            <td onClick={() => play(4)} className="border border-white h-[70px] w-[70px] text-white text-center text-4xl hover:bg-zinc-700 cursor-pointer">{board[4]}</td>
-            <td onClick={() => play(5)} className="border border-white h-[70px] w-[70px] text-white text-center text-4xl hover:bg-zinc-700 cursor-pointer">{board[5]}</td>
+            <td onClick={() => play(3)} className={`${board[3]}`}></td>
+            <td onClick={() => play(4)} className={`${board[4]}`}></td>
+            <td onClick={() => play(5)} className={`${board[5]}`}></td>
           </tr>
           <tr>
-            <td onClick={() => play(6)} className="border border-white h-[70px] w-[70px] text-white text-center text-4xl hover:bg-zinc-700 cursor-pointer">{board[6]}</td>
-            <td onClick={() => play(7)} className="border border-white h-[70px] w-[70px] text-white text-center text-4xl hover:bg-zinc-700 cursor-pointer">{board[7]}</td>
-            <td onClick={() => play(8)} className="border border-white h-[70px] w-[70px] text-white text-center text-4xl hover:bg-zinc-700 cursor-pointer">{board[8]}</td>
+            <td onClick={() => play(6)} className={`${board[6]}`}></td>
+            <td onClick={() => play(7)} className={`${board[7]}`}></td>
+            <td onClick={() => play(8)} className={`${board[8]}`}></td>
           </tr>
         </tbody>
       </table>
+      <audio src="/gameover.mp3"/>
+      <audio src="/win.mp3"/>
     </section>
   )
 }
